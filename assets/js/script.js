efe19ecd1494302f5f4f3ca805c63be97f096c14
KarLOL82 <klinfeldt@gmail.com>
2022-07-30 21:02:09 -0700

    added color changes based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,104 +1,107 @@
-// Current Date / Time
-let currentDateEl = $('#currentDate');
-let currentDate;
-let currentTime;
+// Displays Date
+var currentDateEl = $("#currentDate");
+var currentDate;
+var currentTime;
 
-// Set-To/Get-From Local Storage (Time and Text)
-let calEntryEventTime;
-let calEntryEventTxt;
-let timeArr = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
+// Local Storage Variables
+var eventTimeEL;
+var eventText;
+var timeBlockArray = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
 
-// Button
-let saveBtn = $('.saveBtn');
+// Button to save events
+var saveBtn = $(".saveBtn");
 
 // Determine Color
-let calTimeblock;
-let timerInterval;
-let timeblockID = $("textarea[id*='timeblock']");
+var calTimeblock;
+var timerInterval;
+var timeblockID = $("textarea[id*='timeblock']");
 
 // Calls Functions to Render Date and Events to the DOM & Update Colors
 function init() {
-    currentMomentDate();
-    renderEvents();
-    setBGColors();
-};
+  currentMomentDate();
+  renderEvents();
+  setBGColors();
+}
 
 // Gets Current Date and Renders in Jumbotron Header
 function currentMomentDate() {
-    currentDate = moment().format('dddd, LL');
-    currentDateEl.text(currentDate);
-};
+  currentDate = moment().format("dddd, LL");
+  currentDateEl.text(currentDate);
+}
 
 // Renders Events Pulled from Local Storage to DOM
 function renderEvents() {
-    for (let i = 0; i < timeArr.length; i++) { 
-        $('[id^=timeblock-]').each(function (i, v) {
-            $(v).val(localStorage.getItem(timeArr[i]));
-        })
-    }
-};
+  for (var i = 0; i < timeBlockArray.length; i++) {
+    $("[id^=timeblock-]").each(function (i, v) {
+      $(v).val(localStorage.getItem(timeBlockArray[i]));
+    });
+  }
+}
 
 // Triggers Click Handler for Save Buttons
-saveBtn.on('click', saveButtonClickHandler);
+saveBtn.on("click", saveButtonClickHandler);
 
 // When Save Button Clicked, Pulls Corresponding Time and Date Values
 function saveButtonClickHandler(event) {
-    // Keeps Form from Sending
-    event.preventDefault();
-    // Sets Value to Time Associated with Clicked Save Button
-    calEntryEventTime = $(this).attr('id').split('-')[1];
-    // Sets Value to the User's Input Text
-    calEntryEventTxt = $(this).siblings('textarea[name^="timeblock"]').val().trim();
-    // Calls Function to Store in Local Storage
-    storeEvents();
-};
+  // Keeps Form from Sending
+  event.preventDefault();
+  // Sets Value to Time Associated with Clicked Save Button
+  eventTimeEL = $(this).attr("id").split("-")[1];
+  // Sets Value to the User's Input Text
+  eventText = $(this)
+    .siblings('textarea[name^="timeblock"]')
+    .val()
+    .trim();
+  // Calls Function to Store in Local Storage
+  storeEvents();
+}
 
 // Stores Time and Text Values to Local Storage where (Time = Key) and (User's Input Text = Value)
 function storeEvents() {
-    localStorage.setItem(calEntryEventTime, calEntryEventTxt);
-};
+  window.localStorage.setItem(eventTimeEL, eventText);
+}
 
 // Updates Timeblock Classes/Colors as Time Progresses
 function setBGColors() {
-    // For each timeblock ID, 
-    timeblockID.each(function () {
-    // Split it to display the time contained at the end of the ID, 
-    calTimeBlock = $(this).attr('id').split('-')[1];
+  // For each timeblock ID,
+  timeblockID.each(function () {
+    // Split it to display the time contained at the end of the ID,
+    calTimeBlock = $(this).attr("id").split("-")[1];
     // And convert it to a Moment.js format, then an integer
-    calTimeBlock = parseInt(moment(calTimeBlock, 'H').format('H'));
+    calTimeBlock = parseInt(moment(calTimeBlock, "H").format("H"));
     // Get Moment.js Time & format identically
-    currentTime = parseInt(moment().format('H'));
-    
+    currentTime = parseInt(moment().format("H"));
+
     if (currentTime < calTimeBlock) {
-        $(this).removeClass('past present');
-        $(this).addClass('future');
+      $(this).removeClass("past present");
+      $(this).addClass("future");
     } else if (currentTime === calTimeBlock) {
-        $(this).removeClass('past future');
-        $(this).addClass('present');
+      $(this).removeClass("past future");
+      $(this).addClass("present");
     } else if (currentTime > calTimeBlock) {
-        $(this).removeClass('present future');
-        $(this).addClass('past');
+      $(this).removeClass("present future");
+      $(this).addClass("past");
     } else {
-        console.log("Time Calculation Error");
-        console.log("future");
+      console.log("Time Calculation Error");
+      console.log("future");
     }
-    })
-};
+  });
+}
 
 // Updates Date/Time and Colors Once Per Minute On The Minute
 function setIntervalOnMinute() {
-    var currentDateSeconds = new Date().getSeconds();
-    if (currentDateSeconds == 0) {
-        setInterval(currentMomentDate, 60000);
-        setInterval(setBGColors, 60000);
-    } else {
-        setTimeout(function () {
-            setIntervalOnMinute();
-        }, (60 - currentDateSeconds) * 1000);
-    }
-    currentMomentDate();
-    setBGColors();
-};
+  var currentDateSeconds = new Date().getSeconds();
+  if (currentDateSeconds == 0) {
+    setInterval(currentMomentDate, 60000);
+    setInterval(setBGColors, 60000);
+  } else {
+    setTimeout(function () {
+      setIntervalOnMinute();
+    }, (60 - currentDateSeconds) * 1000);
+  }
+  currentMomentDate();
+  setBGColors();
+}
 
 setIntervalOnMinute();
 
