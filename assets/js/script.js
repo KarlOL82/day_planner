5c5026a4336bcd9550dd1114a123d4d01e0672a4
KarLOL82 <klinfeldt@gmail.com>
2022-08-06 21:17:15 -0700

    going back to original pllan

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var currentTime;
 var eventTimeEL;
 var eventText;
 var timeBlockArray = [9, 10, 11, 12, 13, 14, 15, 16, 17,];
-
+console.log(eventText);
 // Button to save events
 var saveBtn = $(".saveBtn");
 
